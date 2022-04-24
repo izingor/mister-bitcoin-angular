77ab433ba002daacead7abdf6f8385d8cc90c9d2
Igor <izingor@gmail.com>
2022-04-24 15:43:58 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './cmps/app-root/app.component';
+import { HeaderComponent } from './cmps/header/header.component';
+import { FooterComponent } from './cmps/footer/footer.component';
+import { BitcoinAppComponent } from './pages/bitcoin-app/bitcoin-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BitcoinAppComponent
   ],
   imports: [
     BrowserModule,
